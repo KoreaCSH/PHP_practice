2a88156948324405a8e23cc5e7a0bce92fc63c5f
KoreaCSH <tmdgjs7495@naver.com>
2022-05-23 16:08:46 +0900

    index

---
+++ index.php
@@ -0,0 +1,24 @@
+<? include ("header.php"); ?>
+
+<style>
+
+	.navbar { z-index: 999; }
+	
+	p { padding : 0px 30px 0px 30px; text-align: center; }
+	
+	h1 { padding : 0px 30px 0px 30px; text-align: center; font-weight: 800; }
+	
+	.container { position : relative; top: 10px; background : rgba(255, 255, 255, .9); }
+				
+	.ref { font-weight : 200; font-size : 0.5em; }
+	
+</style>
+
+<div class="container">
+	<p>dd</p>
+	<h1>고려증권</h1>
+	<p>유가증권시장에 상장된 종목을 매수-매도할 수 있는 웹 입니다.<br>
+	<b>바로 오늘 시작하세요!</b></p>
+</div>
+
+<? include ("footer.php"); ?>
